07ed3104caed90b730650a2c4645183be98ad081
Alaina Kurt <alaina.c.kurt-1@ou.edu>
2021-02-18 18:08:02 -0600

    Added function myplot for lab 4; altered DESCRIPTION.

---
+++ R/myplot.R
@@ -0,0 +1,12 @@
+#' @title Vector for a lab 4 plot
+#'
+#' @param x A quantitative vector
+#'
+#' @return y values of plot
+#' @export
+#'
+#' @examples
+#' \dontrun{d <- 1:40; myplot(x=d)}
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
